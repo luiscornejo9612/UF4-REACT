92f7ab24c11f3c88d9f589c60a7d221d3a29299a
luiscornejo9612 <luisalvarado338@gmail.com>
2024-04-14 22:09:32 +0200

    practica tutorial terminado

---
+++ pages/Auth/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login Content</div>
+  )
+}
+
+export default Login
---
+++ pages/Auth/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>Register Content</div>
+  )
+}
+
+export default Register
